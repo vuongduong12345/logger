194702d1dbddf46978b92e81801f2dc4a2b0b75c
vuongduong12345 <vuongtatduong@gmail.com>
2020-01-07 16:42:52 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueLogger from 'vuejs-logger'
 
-Vue.config.productionTip = false
+const isProduction = process.env.NODE_ENV === 'production';
+const options = {
+  isEnabled: true,
+  logLevel : isProduction ? 'error' : 'debug',
+  stringifyArguments : false,
+  showLogLevel : true,
+  showMethodName : true,
+  separator: '|',
+  showConsoleColors: true
+};
+
+Vue.use(VueLogger, options)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  data() {
+    return{
+      a : 'a',
+      b : 'b'
+    }
+  },
+  created() {
+    this.$log.debug('test', this.a, 123)
+    this.$log.info('test', this.b)
+    this.$log.warn('test')
+    this.$log.error('error')
+    this.$log.fatal('test')
+}
+ 
+})
